19f624596302ac958bd36af7512fae31d9a266b7
ajaidan0 <ajaidan0@gmail.com>
2020-08-11 01:10:42 -0400

    add new thingy

---
+++ commit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf */.theos
+rm -rf */packages
+git add .
+
+echo "Please put your commit message below"
+read -p ">>> " MESSAGE
+NEWMESSAGE="$(echo $MESSAGE)"
+git commit -m "$NEWMESSAGE"
+git push
